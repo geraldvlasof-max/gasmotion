f4867972a5f7382a8937018586b0b9be541035cb
geraldvlasof-max <geraldvlasof@gmail.com>
2026-06-17 21:42:47 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Gasmotion Service Worker v5.1
-const CACHE_NAME = 'gasmotion-v51';
+// Gasmotion Service Worker v5.2
+const CACHE_NAME = 'gasmotion-v52';
 const WORKER_DOMAIN = 'gasmotion-sync.geraldvlasof.workers.dev';
 self.addEventListener('install', function(e) { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
